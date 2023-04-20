91e2db892201e8b74471f11129255f5254d3e81c
Rutwikwr <rutwikwadhankar@gmailcom>
2023-04-20 14:07:14 +0530

    noteapp

---
+++ app/src/main/java/com/gmail/notes/NoteDao.kt
@@ -0,0 +1,17 @@
+package com.gmail.notes
+
+import android.renderscript.Sampler
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+      @Insert(onConflict = OnConflictStrategy.IGNORE)
+      suspend fun insert(note: Note)
+
+      @Delete
+      suspend fun delete(note: Note)
+
+      @Query("Select * from `notes table` order by id ASC")
+      fun getAllNote(): LiveData<List<Note>>
+}
